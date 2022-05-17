262b18d4fa73a96b0079dcc7fd843bc1fc316c3d
Pedro Eduardo Guimaraes de Oliveira Gome - 320138686 <320138686@ulife.com.br>
2022-05-17 20:26:54 -0300

    API finishing

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,6 +9,14 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+CORS(app)
+
 from app.models import produto
 
 db.create_all()
+
+# importar as classes controladoras
+from app.controllers.produto_controller import ProdutoController
+app.register_blueprint(ProdutoController.produto_controller, url_perfilx="/api/v1/")
+
+
